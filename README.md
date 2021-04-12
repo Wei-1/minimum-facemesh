bbd98508e7a484133f28713f771544fea07f8675
Wei Chen <wcheamz@amazon.com>
2021-04-12 19:14:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Minimal Face Mesh
+
+This project uses TF.js to calculate the facial landmarks.
+
+[DEMO](https://wei-1.github.io/minimum-facemesh/)
+
+There are many Demos out there that are too complex. This repository is the minimum setup that I developed that contains a reasonable amount of component.
+
+This repo uses that latest dependencies from Google at 2021-04-12:
+ - tfjs@3.3.0
+ - face-landmarks-detection@0.0.3
+
+`We see more than 10% frame rate improvement jumping from TF.js 2.4.0 to TF.js 3.3.0.`
+
+If you want to try this repo by yourself, you can clone the project and use [http-server](https://www.npmjs.com/package/http-server) to host the `index.html` file.
+
---
+++ sketch.js
@@ -0,0 +1,116 @@
+// sketch.js
+
+// config
+var MAX_FACES = 1;
+var facemeshModel = null; // this will be loaded with the facemesh model
+var videoDataLoaded = false; // is webcam capture ready?
+var statusText = "Loading facemesh model...";
+var myFaces = []; // faces detected in this browser
+var canvasRatio = 0.5;
+
+// html canvas for drawing debug view
+var dbg = document.createElement("canvas").getContext('2d');
+dbg.canvas.style.position = "absolute";
+dbg.canvas.style.left = "0px";
+dbg.canvas.style.top = "0px";
+dbg.canvas.style.zIndex = 100; // "bring to front"
+document.body.appendChild(dbg.canvas);
+
+// read video from webcam
+var capture = document.createElement("video");
+capture.playsinline = "playsinline";
+capture.autoplay = "autoplay";
+navigator.mediaDevices.getUserMedia({audio:false, video:{
+  facingMode: 'user',
+  width: undefined,
+  height: undefined,
+}}).then(function(stream){
+  window.stream = stream;
+  capture.srcObject = stream;
+});
+
+// hide the video element
+capture.style.position = "absolute";
+capture.style.opacity = 0;
+capture.style.zIndex = -100; // "send to back"
+
+// signal when capture is ready and set size for debug canvas
+capture.onloadeddata = function(){
+  console.log("video initialized");
+  videoDataLoaded = true;
+  dbg.canvas.width = Math.floor(capture.videoWidth * canvasRatio);
+  dbg.canvas.height = Math.floor(capture.videoHeight * canvasRatio);
+}
+
+// load the MediaPipe facemesh model assets.
+faceLandmarksDetection.load().then(function(_model){
+  console.log("model initialized.");
+  statusText = "Model loaded.";
+  facemeshModel = _model;
+});
+
+// draw a face object (2D debug view) returned by facemesh
+function drawFaces(faces, noKeypoints){
+  for (var i = 0; i < faces.length; i++){
+    const keypoints = faces[i].scaledMesh;
+    for (var j = 0; j < TRI.length; j+=3){
+      var a = keypoints[TRI[j  ]];
+      var b = keypoints[TRI[j+1]];
+      var c = keypoints[TRI[j+2]];
+      dbg.beginPath();
+      dbg.moveTo(a[0], a[1]);
+      dbg.lineTo(b[0], b[1]);
+      dbg.lineTo(c[0], c[1]);
+      dbg.closePath();
+      dbg.stroke();
+    }
+  }
+}
+
+// reduce vertices to the desired set, and compress data as well
+function packFace(face, set){
+  var ret = {scaledMesh:[]};
+  for (var i = 0; i < set.length; i++){
+    var j = set[i];
+    ret.scaledMesh[i] = [
+      Math.floor(face.scaledMesh[j][0] * 100) / 100,
+      Math.floor(face.scaledMesh[j][1] * 100) / 100,
+      Math.floor(face.scaledMesh[j][2] * 100) / 100,
+    ];
+  }
+  return ret;
+}
+
+// the main render loop
+function render() {
+  requestAnimationFrame(render); // this creates an infinite animation loop
+  
+  if (facemeshModel && videoDataLoaded){ // model and video both loaded
+    facemeshModel.pipeline.maxFaces = MAX_FACES;
+    facemeshModel.estimateFaces({input:capture}).then(function(_faces){
+      myFaces = _faces.map(x => packFace(x, VTX)); // update the global myFaces
+      if (!myFaces.length){ // haven't found any faces
+        statusText = "Show Some Faces";
+      }else{ // display the confidence, to 3 decimal places
+        statusText = "Face Detected";
+      }
+    });
+  }
+  
+  dbg.clearRect(0, 0, dbg.canvas.width, dbg.canvas.height);
+  
+  dbg.save();
+  dbg.fillStyle = "red";
+  dbg.strokeStyle = "red";
+  dbg.scale(canvasRatio, canvasRatio);
+  dbg.drawImage(capture, 0, 0); // print the camera
+  drawFaces(myFaces); // print the mesh
+  dbg.restore();
+  
+  dbg.save();
+  dbg.fillStyle = "red";
+  dbg.fillText(statusText, 2, 60);
+  dbg.restore();
+}
+
+render(); // kick off the rendering loop!
